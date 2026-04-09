4212f61f7c2dbb1f99767c8c44cdf8e4ea1a0b72
Patrick Millares <f6205106@macmini21.local>
2026-04-09 17:03:45 -0400

    added search functionality

--- A2_iOS_Patrick_101205106/ProductListView.swift
+++ A2_iOS_Patrick_101205106/ProductListView.swift
@@ -11,8 +11,22 @@ struct ProductListView: View {
     @FetchRequest(sortDescriptors: [NSSortDescriptor(keyPath: \Product.productName, ascending: true)])
     private var products: FetchedResults<Product>
     
+    @State private var searchText = ""
+    
+    // Logic to filter products by Name OR Description
+    var filteredProducts: [Product] {
+        if searchText.isEmpty {
+            return Array(products)
+        } else {
+            return products.filter {
+                ($0.productName ?? "").localizedCaseInsensitiveContains(searchText) ||
+                ($0.productDescription ?? "").localizedCaseInsensitiveContains(searchText)
+            }
+        }
+    }
+    
     var body: some View {
-        List(products) { product in
+        List(filteredProducts) { product in
             VStack(alignment: .leading) {
                 Text(product.productName ?? "Unknown Product")
                     .font(.headline)
@@ -23,5 +37,6 @@ struct ProductListView: View {
             }
         }
         .navigationTitle("All Products")
+        .searchable(text: $searchText, prompt: "Search name or description")
     }
 }
